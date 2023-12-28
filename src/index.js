fd5e6d0ad837c721a04d0b150ae85c5e0c836682
lernde <gopiyadav1715@gmail.com>
2023-12-28 15:02:26 +0530

    removed unnecessary files

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Sidenav from './components/Sidenav';
 import "rsuite/dist/rsuite.min.css";
-import Test from './components/Test';
-import Mainpage from './components/Mainpage';
-import Firstpage from './components/Firstpage';
 import Initial from './components/Initial';
 import Student from './components/Student';
 import Registeration from './components/Registeration';
@@ -14,10 +10,6 @@ function MainContainer() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='Mainpage' element={<Mainpage />} />
-        <Route path='Sidenav' element={<Sidenav />} />
-        <Route path='Test' element={<Test />} />
-        <Route path='Firstpage' element={<Firstpage />} />
         <Route path='Initial' element={<Initial />} />
         <Route path='Registeration/:studentMail?' element={<Registeration />} />
         <Route path='Student/:studentMail?' element={<Student />} />
